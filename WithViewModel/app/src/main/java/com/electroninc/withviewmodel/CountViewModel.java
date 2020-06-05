7f0d2e75cf8eec8a0e5eae2bc52dddaea55769d4
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-05 23:24:11 +0100

    Finished WithViewModel

---
+++ WithViewModel/app/src/main/java/com/electroninc/withviewmodel/CountViewModel.java
@@ -0,0 +1,7 @@
+package com.electroninc.withviewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class CountViewModel extends ViewModel {
+    public int count = 0;
+}
--- WithViewModel/app/src/main/java/com/electroninc/withviewmodel/MainActivity.java
+++ WithViewModel/app/src/main/java/com/electroninc/withviewmodel/MainActivity.java
@@ -1,14 +1,38 @@
 package com.electroninc.withviewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CountViewModel countViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countViewModel = ViewModelProviders.of(this).get(CountViewModel.class);
+
+        final TextView countTextView = findViewById(R.id.count_text_view);
+        countTextView.setText(formatCount());
+
+        Button addOneButton = findViewById(R.id.add_one_btn);
+        addOneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countViewModel.count++;
+                countTextView.setText(formatCount());
+            }
+        });
+    }
+
+    private String formatCount() {
+        return "Count: " + countViewModel.count;
     }
 }
