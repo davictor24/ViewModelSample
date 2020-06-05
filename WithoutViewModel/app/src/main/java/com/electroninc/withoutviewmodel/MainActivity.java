9566c2179668fe131d735ba1ea7fb9570b65ceaf
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-05 23:01:20 +0100

    Finished WithoutViewModel

--- WithoutViewModel/app/src/main/java/com/electroninc/withoutviewmodel/MainActivity.java
+++ WithoutViewModel/app/src/main/java/com/electroninc/withoutviewmodel/MainActivity.java
@@ -3,12 +3,33 @@ package com.electroninc.withoutviewmodel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView countTextView = findViewById(R.id.count_text_view);
+        countTextView.setText(formatCount());
+
+        Button addOneButton = findViewById(R.id.add_one_btn);
+        addOneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                countTextView.setText(formatCount());
+            }
+        });
+    }
+
+    private String formatCount() {
+        return "Count: " + count;
     }
 }
